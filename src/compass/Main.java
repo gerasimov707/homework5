67694825b1b7752d5a54529a10771ffe58bb77d3
alexandr <gerasimov707@yandex.ru>
2022-04-10 19:59:26 +0300

    сделал 1 и 2 задание

--- src/compass/Main.java
+++ src/compass/Main.java
@@ -1,8 +1,69 @@
 package compass;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        //задание 1
+        int[] a = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        System.out.print(a[0] + ",");
+        System.out.print(a[1] + ",");
+        System.out.print(a[2]);
+        System.out.print("\n");
+        double[] b = new double[3];
+        b[0] = 1.57;
+        b[1] = 7.654;
+        b[2] = 9.986;
+        System.out.print(b[0] + ",");
+        System.out.print(b[1] + ",");
+        System.out.print(b[2]);
+        System.out.print("\n");
+        int[] mass = {2, 5, 77};
+        for (int i = 0; i < mass.length; i++) {
+            if (i == mass.length - 1)
+                System.out.println(mass[i]);
+            else
+                System.out.print(mass[i] + ",");
+        }
+        //задание 2
+        int[] a1 = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        System.out.print(a[0] + ",");
+        System.out.print(a[1] + ",");
+        System.out.print(a[2] + "   ");
+        double[] b1 = new double[3];
+        b[0] = 1.57;
+        b[1] = 7.654;
+        b[2] = 9.986;
+        System.out.print(b[0] + ",");
+        System.out.print(b[1] + ",");
+        System.out.print(b[2] + "   ");
+        int[] mass1 = {2, 5, 77};
+        for (int u = 0; u < mass.length; u++) {
+            if (u == mass.length - 1)
+                System.out.println(mass[u]);
+            else
+                System.out.print(mass[u] + ",");
+        }
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
